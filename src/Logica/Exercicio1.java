a24bcc81c657da2c60417c499ca68cd2205e2cd2
David <David@DESKTOP-CHO663R>
2020-10-16 22:43:11 -0300

    primeiro commit

---
+++ src/Logica/Exercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * Declare pelo menos 3 variáveis do tipo inteiro, 3 do tipo real e 3 do 
+ * tipo caracter.
+ */
+package Logica;
+
+/**
+ *
+ * @author David
+ */
+public class Exercicio1 {
+  int idade1;
+  int idade2;
+  int idade3;
+  double salario1;
+  double salario2;
+  double salario3;
+  String nome1;
+  String nome2;
+  String nome3;
+}
