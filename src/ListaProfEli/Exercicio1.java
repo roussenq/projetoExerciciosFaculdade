72dd4134662bd50e0cf51f56338e44e10df1ebbc
David <David@DESKTOP-CHO663R>
2020-10-17 00:21:32 -0300

    primeiro commit2

---
+++ src/ListaProfEli/Exercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * Lista 1 - Logica. 
+ * Execicio 10
+ * Faça um programa que leia o nome, a idade, o salario e o sexo de uma pessoa.
+ * Imprimir a idade se for homem e o salario se dor uma mulher.
+ */
+package ListaProfEli;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author David
+ */
+public class Exercicio1 {
+  String nome = "";
+  int idade;
+  String sexo = "";
+  
+  
+}
---
+++ src/WebEducando/SemOO.java
@@ -0,0 +1,20 @@
+/*
+ * Criar um aplitavo para calcular a area de um triangulo sem utilizar OO.
+ */
+package WebEducando;
+
+/**
+ *
+ * @author David
+ */
+public class SemOO {
+    
+    double xA, xB, xC, yA, yB, yC;
+    
+    sout
+    
+    if {
+    
+}
+            
+}
